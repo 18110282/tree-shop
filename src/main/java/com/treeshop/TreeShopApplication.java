51bf29a1831007e0584b0baffa9517b1b25c207a
18110282 <hieuho10022000@gmail.com>
2022-03-07 16:48:46 +0700

    Create MVN

---
+++ src/main/java/com/treeshop/TreeShopApplication.java
@@ -0,0 +1,13 @@
+package com.treeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treeshop/TreeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
