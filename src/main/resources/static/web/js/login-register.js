3a3869dc7973f0e308a7c0b36db0bdb4cc9d09ab
18110282 <hieuho10022000@gmail.com>
2022-04-21 17:56:19 +0700

    Create interface of Register Form and Send Verifycation Form

--- src/main/resources/static/web/js/login-register.js
+++ src/main/resources/static/web/js/login-register.js
@@ -12,10 +12,33 @@ function showRegisterForm(){
         $('.login-footer').fadeOut('fast',function(){
             $('.register-footer').fadeIn('fast');
         });
-        $('.modal-title').html('Register with');
-    }); 
+        $('.social').fadeIn('fast');
+        $('.division').fadeIn('fast');
+        $('.modal-title').html('&emsp;&ensp;Đăng ký tài khoản');
+
+    });
+    $('.sendVerifyBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.verify-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('&emsp;&ensp;Đăng ký tài khoản');
+    });
     $('.error').removeClass('alert alert-danger').html('');
        
+}
+function showSendVerifyForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.sendVerifyBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.verify-footer').fadeIn('fast');
+        });
+        $('.social').fadeOut('fast');
+        $('.division').fadeOut('fast');
+        $('.modal-title').html('&emsp;&ensp;Xác thực tài khoản');
+    });
+    $('.error').removeClass('alert alert-danger').html('');
+
 }
 function showLoginForm(){
     $('#loginModal .registerBox').fadeOut('fast',function(){
@@ -23,26 +46,44 @@ function showLoginForm(){
         $('.register-footer').fadeOut('fast',function(){
             $('.login-footer').fadeIn('fast');    
         });
+        $('.social').fadeIn('fast');
+        $('.division').fadeIn('fast');
         
-        $('.modal-title').html('Login with');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+        $('.modal-title').html('&emsp;&emsp;Đăng nhập');
+    });
+    $('#loginModal .sendVerifyBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.verify-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');
+        });
+
+        $('.modal-title').html('&emsp;&emsp;Đăng nhập');
+    });
+    $('.error').removeClass('alert alert-danger').html('');
 }
 
 function openLoginModal(){
     showLoginForm();
     setTimeout(function(){
         $('#loginModal').modal('show');    
-    }, 230);
+    }, 150);
     
 }
 function openRegisterModal(){
     showRegisterForm();
     setTimeout(function(){
         $('#loginModal').modal('show');    
-    }, 230);
+    }, 150);
     
 }
+function openSendVerifyForm(){
+    showSendVerifyForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');
+    }, 150);
+
+}
+
 
 function loginAjax(){
     /*   Remove this comments when moving to server
