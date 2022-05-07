0735b8103d705e845cf6a77aef7814695433891e
18110282 <hieuho10022000@gmail.com>
2022-05-07 12:52:43 +0700

    Add common service

---
+++ src/main/java/com/treeshop/service/CommonService.java
@@ -0,0 +1,27 @@
+package com.treeshop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class CommonService {
+    //Process Input File for product, category
+    public void processFile(Path uploadPath,  MultipartFile multipartFile, String fileName) throws IOException {
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new IOException("Could not save uploaded file: " + fileName);
+        }
+    }
+}
+
