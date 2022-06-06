9f9e75f4165f8df1026c67ccb1e98985595e1727
Hồ Hiếu <hieuho10022000@gmail.com>
2022-06-06 10:19:41 +0700

    update for secret information

--- src/main/java/com/treeshop/TreeShopApplication.java
+++ src/main/java/com/treeshop/TreeShopApplication.java
@@ -1,13 +1,27 @@
 package com.treeshop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TreeShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TreeShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TreeShopApplication.class, args);
+        //See all bean
+        //Arrays.stream(app.getBeanDefinitionNames()).forEach(System.out::println);
+    }
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(){
+//        return args -> {
+//          System.out.println("Hello World");
+//        };
+//    }
 
 }
