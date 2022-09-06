3e59992d2203e3e68a125dc41f73b217d13af6c6
Hồ Hiếu <hieuho10022000@gmail.com>
2022-09-07 06:51:26 +0700

    user information interface

---
+++ src/main/java/com/treeshop/controller/client/InformationController.java
@@ -0,0 +1,15 @@
+package com.treeshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/user/information")
+public class InformationController {
+
+    @GetMapping("")
+    public String viewHomeInformation(){
+        return "/views/client/information";
+    }
+}
